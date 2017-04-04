fafef352fcae2e26a5a692d1a3ef6b89614f9900
destruc7i0n <dscdsouza@outlook.com>
2017-04-03 20:30:54 -0400

    Changes * Only add logger in development

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import App from './App'
 import reducer from './reducers'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
-import logger from 'redux-logger'
 
-const store = createStore(
-  reducer,
-  applyMiddleware(logger)
-)
+let store
+
+if (process.env.NODE_ENV === 'development') {
+  // only add logger in development
+  const { logger } = require('redux-logger')
+
+  store = createStore(
+    reducer,
+    applyMiddleware(logger)
+  )
+} else {
+  store = createStore(
+    reducer
+  )
+}
 
 ReactDOM.render(
   <Provider store={store}>
