a9a6e3876a57afaca96214176f92b291d33af31f
destruc7i0n <dscdsouza@outlook.com>
2017-04-04 23:25:41 -0400

    Changes * Use an ugly fix that binds to the window resize method    * Need to replace with CSS        * one day *sigh*

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React, { Component }  from 'react'
 import { Col, Row } from 'react-bootstrap'
 
 import { DragDropContext } from 'react-dnd'
@@ -6,6 +6,8 @@ import MultiBackend from 'react-dnd-multi-backend'
 import HTML5toTouch from 'react-dnd-multi-backend/lib/HTML5toTouch'
 
 import 'babel-polyfill'
+
+import { debounce } from 'lodash'
 
 import Navbar from './components/Navbar'
 import CraftingTable from './components/CraftingTable'
@@ -21,22 +23,48 @@ import './App.css'
 import './assets/arrow.png'
 import './assets/Minecraft.woff'
 
-const App = () =>
-  <div className="container">
-    <Navbar />
-    <IngredientDragLayer />
-    <Row>
-      <Col md={6}>
-        <CraftingTable />
-      </Col>
-      <Col md={6} className="ingredients-col">
-        <Ingredients />
-      </Col>
-      <Col md={6}>
-        <Options />
-        <Output />
-      </Col>
-    </Row>
-  </div>
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleResize = this.handleResize.bind(this)
+  }
+
+  // bad hack, remove soon
+  handleResize() {
+    return debounce(() => this.forceUpdate(), 100)
+  }
+
+  componentDidMount () {
+    window.addEventListener('resize', this.handleResize())
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.handleResize);
+  }
+  // end bad hack
+
+  render () {
+    const isMobile = window.matchMedia && window.matchMedia('only screen and (max-width : 992px)').matches
+
+    return (
+      <div className="container">
+        <Navbar />
+        <IngredientDragLayer />
+        <Row>
+          <Col md={6} sm={12}>
+            <CraftingTable />
+            { isMobile ? <Ingredients /> : null }
+            <Options />
+            <Output />
+          </Col>
+          <Col md={6} sm={12} className="pull-right">
+            { !isMobile ? <Ingredients /> : null }
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
 
 export default DragDropContext(MultiBackend(HTML5toTouch))(App)
