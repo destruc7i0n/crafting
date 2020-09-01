6dc70a7cb8b7375487810263ff017f042e9676f0
destruc7i0n <dscdsouza@outlook.com>
2020-09-01 09:58:01 -0400

    0 !== undefined (again)

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,24 +2,33 @@ import React from 'react'
 
 import './Tooltip.css'
 
-const Tooltip = ({ style, ingredient, title, hidden }) => !hidden ? (
-  <div
-    className='mc-tooltip'
-    style={{
-      display: style.display,
-      top: style.y,
-      left: style.x
-    }}
-  >
-    <div className='mc-tooltip-title'>
-      {title}
+const Tooltip = ({ style, ingredient, title, hidden }) => {
+  let data = 0
+  let hasData = ingredient.hasCustomData()
+  if (hasData) {
+    data = ingredient.getCustomData().data
+    hasData = data !== undefined && !isNaN(data)
+  }
+
+  return !hidden ? (
+    <div
+      className='mc-tooltip'
+      style={{
+        display: style.display,
+        top: style.y,
+        left: style.x
+      }}
+    >
+      <div className='mc-tooltip-title'>
+        {title}
+      </div>
+      <div className='mc-tooltip-description'>
+        {ingredient.id}
+        {/* show the custom data as well for bedrock items */}
+        {hasData ? ':' + data : ''}
+      </div>
     </div>
-    <div className='mc-tooltip-description'>
-      {ingredient.id}
-      {/* show the custom data as well for bedrock items */}
-      {ingredient.hasCustomData() && !!ingredient.getCustomData().data ? ':' + ingredient.getCustomData().data : ''}
-    </div>
-  </div>
-) : null
+  ) : null
+}
 
 export default Tooltip
