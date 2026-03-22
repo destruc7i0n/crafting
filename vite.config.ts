2817d9d2410284eca8de95d37f91ecb6072e3ef7
destruc7i0n <destruc7i0n@users.noreply.github.com>
2026-03-21 20:31:11 -0700

    set build target

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  build: {
+    target: "es2020",
+  },
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
