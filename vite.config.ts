e20d21eec4def22ff8f24f118ce8130c984eb5f7
destruc7i0n <destruc7i0n@users.noreply.github.com>
2026-04-24 17:17:37 -0700

    improve seo tags

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
+import { seo } from "./vite/plugins/seo";
+
 export default defineConfig({
   build: {
     target: "es2020",
   },
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), seo(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
