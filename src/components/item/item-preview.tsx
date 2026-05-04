cb9dac3ed456b9863de74a1c92df1610e888caa2
destruc7i0n <destruc7i0n@users.noreply.github.com>
2026-05-03 19:31:56 -0700

    make item preview stateless

--- src/components/item/item-preview.tsx
+++ src/components/item/item-preview.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, forwardRef, memo, useEffect, useState } from "react";
+import { ComponentPropsWithoutRef, forwardRef, memo } from "react";
 
 import { NoTextureTexture } from "@/data/constants";
 import { cn } from "@/lib/utils";
@@ -10,36 +10,25 @@ type IngredientProps = {
 
 export const ItemPreview = memo(
   forwardRef<HTMLImageElement, IngredientProps>(
-    ({ texture, alt, className, decoding, onError, ...props }, ref) => {
-      const resolvedTexture = texture || NoTextureTexture;
-      const [failedTexture, setFailedTexture] = useState<string | null>(null);
-
-      useEffect(() => {
-        setFailedTexture(null);
-      }, [resolvedTexture]);
-
-      const src = failedTexture === resolvedTexture ? NoTextureTexture : resolvedTexture;
-
-      return (
-        <img
-          {...props}
-          ref={ref}
-          src={src}
-          alt={alt}
-          decoding={decoding ?? "async"}
-          onError={(event) => {
-            if (src !== NoTextureTexture) {
-              setFailedTexture(resolvedTexture);
-            }
-            onError?.(event);
-          }}
-          className={cn(
-            "h-[32px] w-[32px] select-none [-webkit-touch-callout:none] [image-rendering:crisp-edges] [image-rendering:pixelated]",
-            className,
-          )}
-        />
-      );
-    },
+    ({ texture, alt, className, decoding, onError, ...props }, ref) => (
+      <img
+        {...props}
+        ref={ref}
+        src={texture || NoTextureTexture}
+        alt={alt}
+        decoding={decoding ?? "async"}
+        onError={(event) => {
+          if (event.currentTarget.src !== NoTextureTexture) {
+            event.currentTarget.src = NoTextureTexture;
+          }
+          onError?.(event);
+        }}
+        className={cn(
+          "h-[32px] w-[32px] select-none [-webkit-touch-callout:none] [image-rendering:crisp-edges] [image-rendering:pixelated]",
+          className,
+        )}
+      />
+    ),
   ),
 );
 
